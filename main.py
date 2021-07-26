9f760e52f90acd8ff3252446a58881eb7606de43
lizard <shotup0101@gmail.com>
2021-07-26 08:55:30 +0800

    v1.0

--- main.py
+++ main.py
@@ -7,33 +7,48 @@ import json
 import os
 import math
 
+game_name = "League of Legends.exe"
+test_name = "LINE.exe"
+
 
 class Auto_Move:
-    def __init__(self, wait_time, loop_time, positions):
-        self.wait_time = wait_time
-        self.loop_time = loop_time
+    def __init__(self, wait_time, loop_time, positions, actions):
+        self.wait_time = float(wait_time)
+        self.loop_time = int(loop_time)
         self.positions = positions
         self.is_playing = False
         self.timeup = False
+        self.actions = actions
+        self.click_wait = self.positions["click-wait"]
+        self.drag_wait = self.positions["drag-wait"]
+        self.move_wait = self.positions["move-wait"]
+        self.walk_wait = self.positions["walk-wait"]
+        self.proc_wait = self.positions["porc-wait"]
+
+    def Press_Mouse(self, right=False):
+        button = "right" if right else "left"
+        pyautogui.mouseDown(button=button)
+        time.sleep(self.click_wait)
+        pyautogui.mouseUp()
 
     def CountDown(self):
-        print("將於 分鐘後進行投降".format(self.wait_time))
+        print("將於 {}分鐘後進行投降".format(self.wait_time))
         time.sleep(math.floor(self.wait_time * 60))
         self.timeup = True
         print("開始投降")
 
     def Start_Loop(self):
         loop_cnt = 0
-        
+
         # 三個狀態 三個線程
         queue_t = threading.Thread(target=self.Do_Queue)
         play_t = threading.Thread(target=self.Do_Play)
         countdown_t = threading.Thread(target=self.CountDown)
-        
+
         # 依照使用者輸入 進行場次的對戰
         while loop_cnt < self.loop_time:
             for proc in psutil.process_iter():
-                if proc.name() == "League of Legends.exe" and not self.is_playing:
+                if proc.name() == game_name and not self.is_playing:
                     # 找到這個執行緒 代表遊戲開始
                     queue_t.do_run = False
                     self.is_playing = True
@@ -41,17 +56,29 @@ class Auto_Move:
             if self.is_playing:
                 # 跑等投降
                 if not play_t.is_alive():
+                    play_t = threading.Thread(target=self.Do_Play)
+                    countdown_t = threading.Thread(target=self.CountDown)
+                    self.timeup = False
                     countdown_t.start()
                     play_t.do_run = True
                     play_t.start()
-                    loop_cnt = loop_cnt + 1
                 elif self.timeup:
                     # 如果正在玩 且 正在等待投降 且 時間到了
                     play_t.do_run = False
+                    tmp = True
+                    while tmp or play_t.is_alive():
+                        tmp = False
+                        for proc in psutil.process_iter():
+                            if proc.name() == game_name:
+                                tmp = True
+                        time.sleep(self.proc_wait)
+
+                    loop_cnt = loop_cnt + 1
 
             else:
                 # 跑列隊
                 if not queue_t.is_alive():
+                    queue_t = threading.Thread(target=self.Do_Queue)
                     print("第 {} 場掛機".format(loop_cnt))
                     queue_t.do_run = True
                     queue_t.start()
@@ -61,52 +88,118 @@ class Auto_Move:
         print("開始列隊")
         t = threading.currentThread()
 
+        room_cnt = 0
+        self.MoveTo(self.positions["room"])
+        self.Press_Mouse()
         while getattr(t, "do_run", True):
             # 點擊 開始遊戲
-            pyautogui.moveTo(
-                self.positions["new_game"][0], self.positions["new_game"][1]
-            )
-            pyautogui.click()
-            time.sleep(5)
+            self.MoveTo(self.positions["new_game"])
+            self.Press_Mouse()
             # 點擊 開始遊戲
-            pyautogui.moveTo(self.positions["start"][0], self.positions["start"][1])
-            pyautogui.click()
-            time.sleep(5)
+            self.MoveTo(self.positions["start"])
+            self.Press_Mouse()
 
             # 點擊 接受對戰
-            pyautogui.moveTo(self.positions["accept"][0], self.positions["accept"][1])
-            pyautogui.click()
-            time.sleep(5)
+            self.MoveTo(self.positions["accept"])
+            self.Press_Mouse()
+
+            if room_cnt > 10:
+                self.MoveTo(self.positions["room"])
+                self.Press_Mouse()
+                room_cnt = 0
+            else:
+                room_cnt = room_cnt + 1
 
     def Do_Play(self):
         # 遊玩中
         print("進入對戰")
         t = threading.currentThread()
+        wander_cnt = 0
         while getattr(t, "do_run", True):
-            pyautogui.move(250, 0)
-            time.sleep(5)
-            pyautogui.click(button="right")
-            time.sleep(5)
-            pyautogui.move(-250, 0)
-            time.sleep(5)
-            pyautogui.click(button="right")
-            time.sleep(5)
+            if wander_cnt > (1800):
+                wander_cnt = 0
+                self.Wandering()
+                if not getattr(t, "do_run", True):
+                    break
+            else:
+                wander_cnt = wander_cnt + 1
 
-        print("對戰結束，進行下一場對戰")
-        pyautogui.press("esc")
-        time.sleep(3)
-        pyautogui.moveTo(self.positions["surrender"][0], self.positions["surrender"][1])
-        time.sleep(3)
-        pyautogui.click()
-        time.sleep(3)
-        pyautogui.moveTo(
-            self.positions["sur_accept"][0], self.positions["sur_accept"][1]
-        )
-        time.sleep(3)
-        pyautogui.click()
+            if self.actions == 1:
+                self.D_Card()
+            elif self.actions == 2:
+                self.Shops()
+                if not getattr(t, "do_run", True):
+                    break
+                self.Sell_Card()
+            elif self.actions == 3:
+                self.Get_Exp()
 
+        tmp = True
+        while tmp:
+            tmp = False
+            self.Surrender_Task()
+            for proc in psutil.process_iter():
+                if proc.name() == game_name:
+                    tmp = True
         # 遊戲結束 轉為列隊
         self.is_playing = False
+
+    def Surrender_Task(self):
+        # 移到齒輪
+        self.MoveTo(self.positions["gear"])
+        # 按左鍵
+        self.Press_Mouse()
+        # 移到投降
+        self.MoveTo(self.positions["surrender"])
+        # 按左鍵
+        self.Press_Mouse()
+        # 移到確定投降
+        self.MoveTo(self.positions["sur_accept"])
+        # 按左鍵
+        self.Press_Mouse()
+
+        time.sleep(10)
+
+    def D_Card(self):
+        self.MoveTo(self.positions["d_card"])
+        self.Press_Mouse()
+
+    def Get_Exp(self):
+        self.MoveTo(self.positions["exp"])
+        self.Press_Mouse()
+
+    def Shops(self):
+        for index, card in enumerate(self.positions["shops"]):
+            print("購買 第{}個旗子".format(index + 1))
+            self.MoveTo(card)
+            self.Press_Mouse()
+
+    def Sell_Card(self):
+        for index, card in enumerate(self.positions["owned"]):
+            print("售出 第{}個旗子".format(index + 1))
+            self.MoveTo(card)
+            self.DragTo(self.positions["sell_pos"])
+
+    def Wandering(self):
+        for index, pos in enumerate(self.positions["wandering"]):
+            self.WalkThere(pos)
+            time.sleep(self.walk_wait)
+
+    def MoveTo(self, position):
+        pyautogui.moveTo(position[0], position[1])
+        time.sleep(self.move_wait)
+
+    def WalkThere(self, pos):
+        self.MoveTo(pos)
+        self.Press_Mouse(True)
+
+    def DragTo(self, position):
+        pyautogui.mouseDown()
+        time.sleep(self.drag_wait)
+        pyautogui.moveTo(position)
+        time.sleep(self.drag_wait)
+        pyautogui.mouseUp()
+        time.sleep(self.drag_wait)
 
 
 class Create_Setting:
@@ -176,7 +269,9 @@ if __name__ == "__main__":
         elif val == "3":
             settings = create_setting.Get_Setting()
             print(
-                "開始遊戲: {}\r\n接受遊戲: {}\r\n".format(settings["start"], settings["accept"])
+                "開始遊戲: {}\r\n接受遊戲: {}\r\n齒輪: {}".format(
+                    settings["start"], settings["accept"], settings["gear"]
+                )
             )
         elif val == "1":
             wait_time = input("輸入投降時間 單位: 分鐘\r\n")
@@ -187,9 +282,21 @@ if __name__ == "__main__":
             if loop_time == "":
                 loop_time = 10
 
+            actions = input(
+                "選擇對戰中動作\r\n \
+                                1. D牌\r\n \
+                                2. 購買/售出旗子\r\n \
+                                3. 升級\r\n"
+            )
+            if actions == "":
+                actions = 3
+
             print("本次掛機將在 {}分鐘後進行投降，並且遊玩 {}場".format(float(wait_time), int(loop_time)))
             auto_move = Auto_Move(
-                float(wait_time), int(loop_time), create_setting.Get_Setting()
+                float(wait_time),
+                int(loop_time),
+                create_setting.Get_Setting(),
+                int(actions),
             )
             auto_move.Start_Loop()
         elif val == "4":
