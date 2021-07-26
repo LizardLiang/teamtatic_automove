fd6d0ceefef193b509f1b8bf52d2c01b358a07e9
lizard <shotup0101@gmail.com>
2021-07-26 17:27:19 +0800

    final version

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ test_name = "LINE.exe"
 
 
 class Auto_Move:
-    def __init__(self, wait_time, loop_time, positions, actions):
+    def __init__(self, wait_time, loop_time, positions, actions, calc):
         self.wait_time = float(wait_time)
         self.loop_time = int(loop_time)
         self.positions = positions
@@ -25,6 +25,7 @@ class Auto_Move:
         self.move_wait = self.positions["move-wait"]
         self.walk_wait = self.positions["walk-wait"]
         self.proc_wait = self.positions["proc-wait"]
+        self.calc = calc
 
     def Press_Mouse(self, right=False):
         button = "right" if right else "left"
@@ -92,18 +93,18 @@ class Auto_Move:
         room_cnt = 0
         while not self.is_playing:
             # 點擊 開始遊戲
-            self.MoveTo(self.positions["new_game"])
+            self.MoveTo(self.calc.calc_client(self.positions["new_game"]))
             self.Press_Mouse()
             # 點擊 開始遊戲
-            self.MoveTo(self.positions["start"])
+            self.MoveTo(self.calc.calc_client(self.positions["start"]))
             self.Press_Mouse()
 
             # 點擊 接受對戰
-            self.MoveTo(self.positions["accept"])
+            self.MoveTo(self.calc.calc_client(self.positions["accept"]))
             self.Press_Mouse()
 
             if room_cnt > 10:
-                self.MoveTo(self.positions["room"])
+                self.MoveTo(self.calc.calc_client(self.positions["room"]))
                 self.Press_Mouse()
                 room_cnt = 0
             else:
@@ -156,6 +157,10 @@ class Auto_Move:
         self.MoveTo(self.positions["sur_accept"])
         # 按左鍵
         self.Press_Mouse()
+        # 移至關閉設定
+        self.MoveTo(self.positions["close_setting"])
+        # 按下左鍵
+        self.Press_Mouse()
 
         time.sleep(10)
 
@@ -202,6 +207,47 @@ class Auto_Move:
         time.sleep(self.drag_wait)
 
 
+class Auto_Cal:
+    def __init__(self, client, game):
+        # client
+        # 1800 x 900, 1280 x 720, 1024 x 576
+        if client == 1:
+            self.c_width = 1800
+            self.c_height = 900
+        elif client == 2:
+            self.c_width = 1280
+            self.c_height = 720
+        elif client == 3:
+            self.c_width = 1024
+            self.c_height = 576
+
+        # game
+        # 1920 x 1080, 2560 x 1440, 3840 x 2160
+        if game == 1:
+            self.g_width = 1920
+            self.g_height = 1080
+        elif game == 2:
+            self.g_width = 2560
+            self.g_height = 1440
+        elif game == 3:
+            self.g_width = 3840
+            self.g_height = 2160
+
+        self.w_zero = 1920 / 2
+        self.h_zero = 1080 / 2
+
+    # 計算客戶端按鈕位置
+    def calc_client(self, pos):
+        new_w_zero = self.g_width / 2
+        new_h_zero = self.g_height / 2
+
+        # 原本的距離 x 客戶端的比率 加到新的中心點
+        new_w = (pos[0] - self.w_zero) * (self.c_width / 1280) + new_w_zero
+        new_h = (pos[1] - self.h_zero) * (self.c_height / 720) + new_h_zero
+
+        return [new_w, new_h]
+
+
 class Create_Setting:
     def __init__(self):
         self.file = None
@@ -216,12 +262,19 @@ class Create_Setting:
             print("讀取設定完畢")
             return True
         except:
-            self.file = open("setting.json", "x")
-            print("找不到初始化設定，請先進行設定")
+            print("找不到初始化設定，請重新下載 setting.json")
             return False
 
     def Get_Setting(self):
         return self.setting
+
+    def Write_Setting(self, settings):
+        content = json.dumps(settings)
+
+        f = open("setting.json", "w")
+        f.write(content)
+        f.close()
+        print("已存入新預設值")
 
 
 class Calibrate:
@@ -317,8 +370,12 @@ class Calibrate:
             self.cnt = self.cnt + 1
 
 
+c_reso_msg = ["1800 x 900", "1280 x 760", "1024 x 576"]
+g_reso_msg = ["1920 x 1080", "2560 x 1440", "3840 x 2160"]
+action_msg = ["不動作", "D牌", "購買/售出 棋子", "升級"]
+
 if __name__ == "__main__":
-    print("版本 v1.0 2021/07/23 12.41")
+    print("版本 v1.0 2021/07/23 修改忘記把預設寫入存檔版")
     create_setting = Create_Setting()
     res = create_setting.Open_File()
 
@@ -326,39 +383,76 @@ if __name__ == "__main__":
         create_setting.Record_Pos()
 
     while True:
-        val = input("選擇工作\r\n1. 開始腳本\r\n2. 清除設定\r\n3. 檢視設定\r\n4. 重新校正\r\n5. 離開\r\n")
-        if val == "2":
-            create_setting.Reset_Setting()
-        elif val == "3":
-            settings = create_setting.Get_Setting()
-            print(
-                "開始遊戲: {}\r\n接受遊戲: {}\r\n齒輪: {}".format(
-                    settings["start"], settings["accept"], settings["gear"]
+        val = input("選擇工作\r\n1. 開始腳本\r\n2. 重新校正\r\n3. 離開\r\n")
+        if val == "1":
+            setting = create_setting.Get_Setting()
+            use_default = input("使用預設值(是)?\r\n1. 是\r\n2. 否\r\n")
+            wait_time, loop_time, actions, c_reso, g_reso = "", "", "", "", ""
+
+            if use_default == "2":
+                wait_time = input("輸入投降時間 單位: 分鐘\r\n")
+                if wait_time == "":
+                    wait_time = 13
+
+                loop_time = input("輸入掛機場數\r\n")
+                if loop_time == "":
+                    loop_time = 10
+
+                actions = input(
+                    """選擇對戰中動作\r\n0.不動作\r\n1. D牌\r\n2. 購買/售出旗子\r\n3. 升級\r\n"""
                 )
-            )
-        elif val == "1":
-            wait_time = input("輸入投降時間 單位: 分鐘\r\n")
-            if wait_time == "":
-                wait_time = 13
+                if actions == "":
+                    actions = 3
 
-            loop_time = input("輸入掛機場數\r\n")
-            if loop_time == "":
-                loop_time = 10
+                c_reso = input(
+                    "請選擇客戶端大小\r\n1. 1800 x 900\r\n2. 1280 x 720\r\n3. 1024 x 576\r\n"
+                )
+                if c_reso == "" or int(c_reso) > 3 or int(c_reso) < 1:
+                    c_reso = 2
 
-            actions = input("""選擇對戰中動作\r\n0.不動作\r\n1. D牌\r\n2. 購買/售出旗子\r\n3. 升級\r\n""")
-            if actions == "":
-                actions = 3
+                g_reso = input(
+                    "請選擇螢幕大小\r\n1. 1920 x 1080\r\n2. 2560 x 1440\r\n3. 3840 x 2160\r\n"
+                )
+                if g_reso == "" or int(g_reso) > 3 or int(g_reso) < 1:
+                    g_reso = 1
+
+                values = setting["default"]
+                values["surrender-time"] = float(wait_time)
+                values["loop-times"] = int(loop_time)
+                values["action"] = int(actions)
+                values["client-reso"] = int(c_reso)
+                values["monitor-reso"] = int(g_reso)
+
+                setting["default"] = values
+                create_setting.Write_Setting(setting)
+
+            else:
+                values = setting["default"]
+                wait_time = values["surrender-time"]
+                loop_time = values["loop-times"]
+                actions = values["action"]
+                c_reso = values["client-reso"]
+                g_reso = values["monitor-reso"]
 
             print("本次掛機將在 {}分鐘後進行投降，並且遊玩 {}場".format(float(wait_time), int(loop_time)))
+            print(
+                "設定:\r\n動作: {}\r\n客戶端解析度: {}\r\n螢幕解析度: {}\r\n".format(
+                    action_msg[int(actions)],
+                    c_reso_msg[int(c_reso) - 1],
+                    g_reso_msg[int(g_reso) - 1],
+                )
+            )
+
             auto_move = Auto_Move(
                 float(wait_time),
                 int(loop_time),
-                create_setting.Get_Setting(),
+                setting,
                 int(actions),
+                Auto_Cal(int(c_reso), int(g_reso)),
             )
             auto_move.Start_Loop()
-        elif val == "4":
+        elif val == "2":
             calibrate = Calibrate(create_setting.Get_Setting())
             calibrate.start()
-        elif val == "5":
+        elif val == "3":
             break
